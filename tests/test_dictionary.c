fdea322482bbb093a32c1135430338956e593566
dwahme <dkwhitehead@cs.uchicago.edu>
2018-05-02 21:33:21 -0500

    Added tests for in_dict

--- tests/test_dictionary.c
+++ tests/test_dictionary.c
@@ -37,3 +37,78 @@ Test(dictionary, free)
 
     cr_assert_eq(rc, 0, "dict_free() failed");
 }
+
+/* Test arg 1 for null input for in_dict */
+Test(dictionary, in_dict_null) {
+    dict_t *d;
+    int rc;
+
+    d = dict_new("dict_test0.txt");
+
+    rc = in_dict(NULL, d);
+
+    cr_assert_eq(rc, -1, "Passing NULL in as a string in in_dict should have "
+                 "returned -1, but it returned %d", rc);
+}
+
+/* Test arg 2 for null input for in_dict */
+Test(dictionary, in_dict_null) {
+    int rc;
+
+    rc = in_dict("hi", NULL);
+
+    cr_assert_eq(rc, -1, "Passing NULL in as a dictionary in in_dict should have "
+                 "returned -1, but it returned %d", rc);
+}
+
+/* Test empty string input for in_dict */
+Test(dictionary, in_dict_empty) {
+    dict_t *d;
+    int rc;
+
+    d = dict_new("dict_test0.txt");
+
+    rc = in_dict("", d);
+
+    cr_assert_eq(rc, -1, "Passing an empty string in as a dictionary to in_dict "
+                 "should have returned -1, but it returned %d", rc);
+}
+
+/* Helper function for testing regular input for in_dict */
+check_in_dict(char *file, char *str, int expected) {
+    dict_t *d;
+    int rc;
+    char *s;
+
+    d = dict_new(file);
+    s = malloc(strlen(str) + 1);
+    s = strcpy(s, str);
+
+    rc = in_dict(s, d);
+
+    cr_assert_eq(rc, expected, "String %s in dictionary from %s"
+                 "should return %d but in_dict returned %d",
+                 s, file, expected, rc);
+
+}
+
+/* Test a small string for success */
+Test(dictionary, in_dict_s0) {
+    check_in_dict("dict_test0.txt", "hi", 1);
+}
+
+/* Test a long string for success */
+Test(dictionary, in_dict_s1) {
+    check_in_dict("dict_test0.txt", "dictionaryverysuperduperlongwordyayitssolongwowcrazy", 1);
+}
+
+/* Test a small string for failure */
+Test(dictionary, in_dict_f0) {
+    check_in_dict("dict_test0.txt", "no", 0);
+}
+
+/* Test a long string for failure */
+Test(dictionary, in_dict_f1) {
+    check_in_dict("dict_test0.txt", "nojfkdsjfksdjfdsjfkdsjfjfkdsjfkdsjfkdsjkfiejkdjskfsd", 0);
+}
+
