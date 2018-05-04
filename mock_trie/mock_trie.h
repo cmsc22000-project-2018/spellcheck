42fd6e8f0193aef965c6e55a4eaacbbb53f76aa4
denizturkcapar <dturkcapar@gmail.com>
2018-05-03 21:02:48 -0500

    Update mock_trie.h

--- mock_trie/mock_trie.h
+++ mock_trie/mock_trie.h
@@ -4,18 +4,74 @@
 #define MAX_WORD_SIZE   40
 #define MAX_DESC_SIZE  200
 
-char words[4][10] = {"Blue", "Red"};
 /* A mock-trie struct */
 typedef struct {
     char* words;
 } trie_t;
 
-void is_word(trie_t *t);
 
+/*
+ * Allocates a new trie in the heap.
+ *
+ * Parameters:
+ *  - word: Words in the trie
+ *
+ * Returns:
+ *  - A trie, or NULL if a point
+ *    cannot be allocated
+ */
+trie_t* trie_new(**word);
+
+
+/*
+ * Initializes a trie
+ *
+ * Parameters:
+ *  - new_trie: A trie. Must point to already allocated memory.
+ *
+ *
+ * Returns:
+ *  - 0 on success, 1 if an error occurs.
+ */
+int trie_init(trie_t *new_trie);
+
+
+/*
+ * Frees the resources associated with a trie
+ *
+ * Parameters:
+ *  - trie: A trie. Must point to a point allocated with segment_new
+ *
+ * Returns:
+ *  - Always returns 0.
+ */
+trie_t trie_free(trie_t *trie);
+
+
+/*
+ * Checks if a word is in a trie
+ *
+ * Parameters:
+ *  - trie: a trie.
+ *
+ * Returns:
+ *  1 if word is in the trie
+ *  0 if word is not in the trie
+ */
 int in_trie(char *str, trie_t *t);
 
-
+/*
+ * Adds a word to trie
+ *
+ * Parameters:
+ *  - str: a string
+ *  - t: a trie.
+ *
+ * Returns:
+ *  1 if word is in the trie
+ *  0 if word is not in the trie
+ */
+int add_to_trie(char *str, trie_t *t);
 
 
 #endif
-
