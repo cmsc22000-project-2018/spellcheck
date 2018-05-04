89669952299a17f901e6526c5097981ce2e5edf1
denizturkcapar <dturkcapar@gmail.com>
2018-05-03 21:04:17 -0500

    Wrote test cases for init, new and free

--- mock_trie/test_mock_trie.c
+++ mock_trie/test_mock_trie.c
@@ -1,1 +1,45 @@
-a
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "mock_trie.h"
+
+/* Checks whether a new trie is properly created */
+Test(trie, new)
+{
+    trie_t *new_trie;
+    char** word
+
+    point_t  *center;
+
+    new_trie = trie_new(word);
+
+    cr_assert_not_null(new_trie, "trie_new() failed");
+}
+
+/* Checks whether a trie is initiated properly */
+Test(trie, init)
+{
+    trie_t new_trie;
+
+    rc = trie_init(&new_trie);
+
+    cr_assert_eq(rc, SUCCESS, "trie_init() failed");
+
+}
+
+/* Checks whether the trie is correctly freed */
+Test(trie, free)
+{
+    trie_t *new_trie;
+    char** word;
+
+    int rc;
+
+    new_trie = trie_new(word);
+
+    cr_assert_not_null(new_trie, "trie_new() failed");
+
+    rc = trie_free(new_trie);
+
+    cr_assert_eq(rc, SUCCESS, "trie_free() failed");
+}
