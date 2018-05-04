17375004b309044642eb2e3c57b9108460869d57
Jae Park <jaepark@cs.uchicago.edu>
2018-05-04 01:47:43 -0500

    Commit for files relating to parse_file On a side note: check out the poem in the test.txt file.

---
+++ shell/tests/test_textparser.c
@@ -0,0 +1,15 @@
+/* Text Parsing File
+   Jaewan Park
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+char* extract_words(char* s);
+
+char* extract_lines(char* s);
+
+char** parse_file(file); // reconsider data types for this
+
