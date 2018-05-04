102f77da998bc13484bb1a94ed3b6eca26482624
denizturkcapar <dturkcapar@gmail.com>
2018-05-03 21:03:43 -0500

    Updated some functions

--- mock_trie/mock_trie.c
+++ mock_trie/mock_trie.c
@@ -1,25 +1,57 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "mock_trie.h"
 
-trie_t *trie_initialize(trie_t *new_trie){
-    trie_t *new_trie;
+/*
+* See mock_trie.h
+*/
+int trie_init(trie_t *new_trie){
+    assert(new_trie != NULL);
+
     char **w;
     new_trie->words = malloc(sizeof(char *) * 30);
     return new_trie;
     w = malloc(sizeof(char *) * 30);
     new_trie->words = w;
+    return SUCCESS;
 }
 
-trie_t *trie_new(){
+/*
+* See mock_trie.h
+*/
+trie_t* trie_new(**word){
+    trie_t *new_trie;
+    new_trie = malloc(sizeof(trie_t));
+    int rc;
 
+    if (new_trie == NULL) {
+      error("Could not allocate memory");
+      return NULL;
+    }
+    rc = trie_init(new_trie);
+    if(rc != SUCCESS)
+    {
+        error("Could not initialize trie");
+        return NULL;
+    }
+    return new_trie;
 }
 
-trie_t trie_free(){
+/*
+* See mock_trie.h
+*/
+trie_t trie_free(trie_t *trie){
+  assert(trie != NULL);
 
+  free(trie);
+
+  return SUCCESS;
 }
-
+/*
+* See mock_trie.h
+*/
 int in_trie(char *str, trie_t *t){
   for (i = 0; i < 30 ; i++) {
     if (strcmp(str, trie->words[i]) == 0) {
@@ -29,21 +61,17 @@ int in_trie(char *str, trie_t *t){
 }
 
 
-// checks if a word is in a trie
-// return parameters:
-// 1 if word is in the trie
-// 0 if word is not in the trie
+/*
+* See mock_trie.h
+*/
 int add_to_trie(char *str, trie_t *t){
   if (strcmp(str, 'hi') == 0) {
   char  *string = malloc(sizeof(char) * 3);
     t->words[1] = string;
-  }
-
-    if str == 'hi' return 1;
-  /*
-    if str == t->words return 1;
-    else return 0;
- */
-
+    return 1;
+  } else if(strcmp(str, 'dictionaryverysuperduperlongwordyayitssolongwowcrazy') {
+    char  *string = malloc(sizeof(char) * 3);
+      t->words[1] = string;
+      return 1;
+  } return 0;
 }
-
