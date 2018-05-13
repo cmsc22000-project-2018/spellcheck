73672afa4e4d53eb59ebd8725911d2611ae850de
firatciftci <firatciftci@cs.uchicago.edu>
2018-05-13 00:59:20 -0500

    Added color definitions for 'printf()' statements and changed function names to have a more unified layout

--- include/shellstrings.h
+++ include/shellstrings.h
@@ -1,35 +1,50 @@
+
+/* 
+ * String output functions to be used in the shell
+*/
+
 #ifndef _SHELLSTRINGS_H
 #define _SHELLSTRINGS_H
 
-/* Strings to be used in shell
-   Jaewan Park
-*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/* Colors to be used in strings for the shell strings */
+#define RESET       "\033[0m"               // Reset color
+#define RED         "\033[31m"              // Red 
+#define GREEN       "\033[32m"              // Green
+#define YELLOW      "\033[33m"              // Yellow
+#define BLUE        "\033[34m"              // Blue
+#define WHITE       "\033[37m"              // White
+#define BOLDRED     "\033[1m\033[31m"       // Bold Red
+#define BOLDGREEN   "\033[1m\033[32m"       // Bold Green
+#define BOLDYELLOW  "\033[1m\033[33m"       // Bold Yellow
+#define BOLDBLUE    "\033[1m\033[34m"       // Bold Blue
+#define BOLDWHITE   "\033[1m\033[37m"       // Bold White
 
 /* Home Page */
-void greet();
-
-void main_help_text();
-
-/* Help Page */
-void help_page_text();
+void shell_greeting();
 
 /* Save Page */
-void save_page_text();
+void shell_save();
 
 /* Print error, allowing for description depending on context */
-void error_shell(char* s);
+void shell_error(char* s);
 
 /* Command Line Input Format */
-void usage();
+void shell_usage();
 
 /* Shell Prompt */
 void shell_prompt();
 
 /* Final message */
-void bye();
+void shell_outro();
 
 /* File input in Terminal */
-void input(char* input_file, char* status);
+void shell_input(char* input_file, char* status);
+
+/* Help Page */
+void shell_help();
+
 #endif
