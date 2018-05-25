4e1957a947994d4d6d2708355dab49faeb258efb
firatciftci <firatciftci@cs.uchicago.edu>
2018-05-25 02:09:00 -0500

    Removed helper functions from word.h and tidied up header comments

--- include/word.h
+++ include/word.h
@@ -1,3 +1,7 @@
+/*
+ * A module to handle each word and provide suggestions
+ */
+
 #ifndef INCLUDE_WORD_H_
 #define INCLUDE_WORD_H_
 
@@ -6,52 +10,29 @@
 #include "dictionary.h"
 
 /*
- * valid_word: checks that word is in dictionary
- * parameters: word, dictionary
- * returns: int (EXIT_SUCCESS OR FAILURE)
+ * valid_word - checks that word is in dictionary
+ *
+ * Parameters:
+ *  - dict: A dictionary pointer. Must point to already allocated memory.
+ *  - word: A string.
+ *
+ * Returns:
+ *  - int: EXIT_SUCCESS or EXIT_FAILURE
  */
 int word_valid(dict_t *dict, char* word);
 
-// helpers for generate_suggestions
-
 /*
- * word_cap_status - checks if a character is a letter & is capitalized
- * parameter: letter 
- * returns: boolean (0 if not capitalized, 1 if capitalized)
- */
-//return positive if sth is capitalized
-int word_cap_status(char l);
-
-/*
- * word_check_cap - checks if a word has a type of capitalization
- * parameter: word
- * return cases:
- * -1 if not a word 
- * 0 if not capitalized
- * 1 if first letter is capitalized (note "I" is included here)
- * 2 if everything is capitalized
- * 3 if inconsistent
- */
-int word_check_cap(char *word);
-
-/*
- * word_decap - returns a decapitalized word
- * parameter: word
- * returns: decapitalized word
- */
-char* word_decap(char *word);
-
-/*
- * word_recap - recapitalize words
- * parameter: array of words, number of words in array, flag (from check_cap)
- * returns - recapitalized words
- */
-char** word_recap(char **words, int wnum, int flag); // int nsug, int flag);
-
-/*
- * generate_suggestions: return a list of suggestions
- * parameters: word, dictionary, array list to which suggestions are stored
- * returns: int (EXIT_SUCCESS OR FAILURE). FAILURE if no suggestions generated
+ * generate_suggestions - Returns a list of possible suggestions
+ *
+ * Parameters:
+ *  - dict: A dictionary pointer. Must point to already allocated memory.
+ *  - word: A string (word).
+ *  - suggestions: An array of strings (words).
+ *  - max_edits: Number of maximum edits.
+ *  - amount: Amount of suggestions.
+ *
+ * Returns:
+ *  - int: EXIT_SUCCESS or EXIT_FAILURE (EXIT_FAILURE if no suggestions generated.)
  */
 int generate_suggestions(dict_t *dict, char *word, char **suggestions, int max_edits, int amount);
 
