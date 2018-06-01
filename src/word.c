d036be3535ff40ec145aef5c1077b59cc9fa6fdd
Jaewan Park <jaepark@uchicago.edu>
2018-06-01 01:13:51 -0500

    word.c indentation

--- src/word.c
+++ src/word.c
@@ -9,13 +9,13 @@
 /* See word.h */
 bool valid_word(dict_t* dict, char* shaved_word) {
     if (dict_chars_exists(dict, *shaved_word) == EXIT_SUCCESS && *shaved_word != '\n') {
-	   if(dict_exists(dict, shaved_word) == EXIT_SUCCESS) {
-            return true;
+        if(dict_exists(dict, shaved_word) == EXIT_SUCCESS) {
+           return true;
         } else {
             return false;
         }
-	}
-	return false;
+    }
+    return false;
 }
 
 /* See word.h */
@@ -27,20 +27,20 @@ int generate_suggestions(char* word, dict_t* dict, char **suggestions) {
     }
 
     suggestions[2] = NULL;
-
-	if (strcmp(word, "splling") == 0) {
-		suggestions[0] = "spelling";
-		suggestions[1] = "spilling";
-		return EXIT_SUCCESS;
-	} else if (strcmp(word, "chequer") == 0) {
-    	suggestions[0] = "checker";
-    	suggestions[1] = "cheque";
-    	return EXIT_SUCCESS;;
-	} else if((strcmp(word, "cme") == 0)) {
-    	suggestions[0] = "come";
-    	suggestions[1] = "came";
-    	return EXIT_SUCCESS;;
-	} else if((strcmp(word, "m'y") == 0)) {
+    
+    if (strcmp(word, "splling") == 0) {
+        suggestions[0] = "spelling";
+	    suggestions[1] = "spilling";
+	    return EXIT_SUCCESS;
+    } else if (strcmp(word, "chequer") == 0) {
+        suggestions[0] = "checker";
+        suggestions[1] = "cheque";
+        return EXIT_SUCCESS;;
+    } else if((strcmp(word, "cme") == 0)) {
+        suggestions[0] = "come";
+        suggestions[1] = "came";
+        return EXIT_SUCCESS;;
+    } else if((strcmp(word, "m'y") == 0)) {
         suggestions[0] = "my";
         suggestions[1] = "me";
         return EXIT_SUCCESS;
