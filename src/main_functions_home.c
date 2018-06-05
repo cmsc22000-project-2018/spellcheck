21d65aa60d78b06b5b415c18359892dead950561
firatciftci <firatciftci@cs.uchicago.edu>
2018-06-05 01:28:38 -0500

    Home functions done

--- src/main_functions_home.c
+++ src/main_functions_home.c
@@ -17,7 +17,7 @@ void help_page(bool color) {
 
     // Accept any input in the command line
     parse_read_line();
-    log_trace("exiting the help page.");
+    log_trace("Exiting help page.");
 }
 
 /* See main_functions_home.h */
@@ -27,24 +27,25 @@ bool fileexists(const char *filename) {
     if (stat(filename, &buffer) == 0) {
         return true;
     }
+
     return false;
 }
 
 /* See main_functions_home.h */
 int change_mode(char *arg, bool color) {
-    log_trace("change_mode mode type: %s", arg);
+    log_trace("(change_mode) Mode type: %s.", arg);
     int a = atoi(arg);
 
     if ((a == QUIET_MODE) || (a == VERBOSE_MODE) || (a == INTERACTIVE_MODE)) {
         return a;
     }
 
-    shell_error("change_mode mode type invalid; reverting to interactive mode.", color);
+    shell_error("(change_mode) Mode type invalid; reverting to interactive mode.", color);
+
     /*
      * The default is 3, given this function is only called in main_page(),
      * at which point interactive mode is what user probably intended
      */
-
     return INTERACTIVE_MODE;
 }
 
@@ -56,100 +57,110 @@ void main_page(bool *quit, int *mode, char *filename, char *dict, bool *color) {
 
     while ((*quit) == true) {
         if (print == true) {
-            log_trace("main_page printing main menu.");
+            log_trace("(main_page) Printing the main menu.");
             shell_main_menu(*color);
         }
 
         shell_prompt(*color);
 
+        // Line is being split into tokens
         line = parse_read_line();
-        args = parse_split_line(line);  // line is now split into tokens
+        args = parse_split_line(line);
 
-        if ((args == NULL) || (args[2] != NULL)) { // 3 inputs, or no input: error message
-
-            log_error("main_page rguments non-existent or too many argument input.");
+        // 3 inputs, or error message if no input
+        if ((args == NULL) || (args[2] != NULL)) {
+            log_error("(main_page) Arguments non-existent or too many arguments input.");
             shell_error("Please type in one of the indicated commands.", *color);
 
             print = false;
             *quit = true;
+        }
 
-        } else if (!strcmp(args[0], "h")) { // Print help page, then wait for user input
-
+        // Prints help page, then waits for user input
+        else if (!strcmp(args[0], "h")) {
             help_page(*color);
 
             print = true;
             *quit = true;
+        }
 
-        } else if (!strcmp(args[0], "f")) { // Check valid file path, then exit. If not, redo loop
-
-            if (!fileexists(args[1])) { //file path checking
-                log_error("main_page invalid file input: file given does not exist.");
+        // Checks valid file path, then exits. If not, redoes loop
+        else if (!strcmp(args[0], "f")) {
+            // File path checking
+            if (!fileexists(args[1])) {
+                log_error("(main_page) Invalid file input: File given does not exist.");
                 shell_error("Please enter a valid file path for a new edit target.", *color);
 
                 print = false;
                 *quit = true;
+            }
 
-            } else {
-                log_debug("main_page file input: %s", args[1]);
+            else {
+                log_debug("(main_page) File input: '%s'.", args[1]);
                 strcpy(filename, args[1]);
-                printf("\nFile input is now %s\n", filename);
+                printf("\nFile input is now '%s'.\n", filename);
 
                 print = false;
                 *quit = false;
             }
+        }
 
-        } else if (!strcmp(args[0], "d")) { // dictionary name change
-
-            if (!fileexists(args[1])) { // Check file path validity for dicitonary
-                log_error("main_page dictionary does not exist.");
+        // Dictionary name change
+        else if (!strcmp(args[0], "d")) {
+            // Checks file path validity for dicitonary
+            if (!fileexists(args[1])) {
+                log_error("(main_page) Dictionary does not exist.");
                 shell_error("Please enter a valid file path for a new dictionary.", *color);
 
                 print = false;
                 *quit = true;
+            }
 
-            } else {
-
+            else {
                 strcpy(dict, args[1]);
-                log_info("main_page dictionary file is now %s.",dict);
+                log_info("(main_page) Dictionary file is now '%s'.",dict);
                 shell_input(args[1], "dictionary file", *color);
-
 
                 print = true;
                 *quit = true;
             }
+        }
 
-        } else if (!strcmp(args[0], "m")) { // change mode
-
-            log_info("main_page mode number accepted: %d.\n", atoi(args[1]));
+        // Change mode
+        else if (!strcmp(args[0], "m")) {
+            log_info("(main_page) Mode number accepted: %d.\n", atoi(args[1]));
             *mode = change_mode(args[1], color);
 
             print = true;
             *quit = true;
+        }
 
-        } else if (!strcmp(args[0], "c")) { // color
-
-            log_trace("main_page reverting mode colors.");
+        // Color
+        else if (!strcmp(args[0], "c")) {
+            log_trace("(main_page) Reverting mode colors.");
             *color = !(*color);
             print = true;
+        }
 
-        } else if (!strcmp(args[0], "q")) { // quit
-
+        // Quit
+        else if (!strcmp(args[0], "q")) {
             print = false;
             *quit = false;
-            log_info("main_page quitting program.");
+            log_info("(main_page) Quitting program.");
             *mode = QUIT;
 
             return;
+        }
 
-        } else { // input bad
-
+        // Input bad
+        else {
             shell_error("Invalid command line input.", *color);
-            log_error("main_page invalid command line input.");
+            log_error("(main_page) Invalid command line input.");
             *quit = true;
         }
 
         free(line);
         free(args);
-        log_debug("main_page reed 'line' and 'args'.");
+        log_debug("(main_page) Freed 'line' and 'args'.");
     }
 }
