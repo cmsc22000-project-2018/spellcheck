6fd7bd999d1e0b9b75912b6369b0aa851060cb2c
firatciftci <firatciftci@cs.uchicago.edu>
2018-06-05 01:22:45 -0500

    Batch mode done

--- src/main_functions_batch.c
+++ src/main_functions_batch.c
@@ -17,7 +17,7 @@
 
 /* See main_functions_batch.h */
 char *edit_batch(char *line, dict_t *dict, int verbosity, int lnum) {
-    log_info("edit_batch batch mode started successfully.");
+    log_info("(edit_batch) Batch mode started successfully.");
 
 	char *line_copy = strdup(line);
     int max_no_suggestions = 4;
@@ -26,28 +26,29 @@ char *edit_batch(char *line, dict_t *dict, int verbosity, int lnum) {
     // Generates an empty array where the misspelled words in a line will be stored
     char **misspelled = calloc(strlen(line), sizeof(char *));
     if (misspelled == NULL) {
-        log_fatal("ERROR (edit_batch): calloc() failed");
+        log_fatal("(edit_batch) calloc() failed.");
         exit(0);
     }
 
     // Generates an empty array where the underline will go
     char *underline = (char *)malloc(sizeof(char) * (strlen(line) + 1));
     if (underline == NULL) {
-        log_fatal("ERROR (edit_batch): calloc() failed");
+        log_fatal("(edit_batch) calloc() failed.");
         exit(0);
     }
     underline[0] = '\0';
 
     // Identifies misspelled words and add to misspelled
     parse_string(line, dict, underline, misspelled);
-    log_info("edit_batch file parsing completed.");
+    log_info("(edit_batch) File parsing completed.");
 
     int i = 0;
     int j;
+
     //Replacing words, printing if batch mode
     while (misspelled[i] != NULL) {
         // Generates suggestions
-        char** suggestions = generate_suggestions(dict, misspelled[i], max_edits, max_no_suggestions);
+        char **suggestions = generate_suggestions(dict, misspelled[i], max_edits, max_no_suggestions);
 
         /*
          * If no suggestions are generated:
@@ -55,7 +56,7 @@ char *edit_batch(char *line, dict_t *dict, int verbosity, int lnum) {
          *  - In quiet mode, save the word as is (without corrections).
          */
         if (suggestions == NULL) {
-            suggestions = calloc(max_no_suggestions, sizeof(char*));
+            suggestions = calloc(max_no_suggestions, sizeof(char *));
         }
 
         if (suggestions[0] == NULL) {
@@ -65,23 +66,26 @@ char *edit_batch(char *line, dict_t *dict, int verbosity, int lnum) {
         }
 
         if (verbosity == QUIET_MODE) {
-            log_trace("edit_batch correcting misspelled line.");
+            log_trace("(edit_batch) Correcting misspelled line.");
             if (suggestions[0] != NULL)
                 correct_line(line_copy, misspelled[i], suggestions[0]);
         }
 
         // In verbose mode, edit the file and also print a replacement chart
         if (verbosity == VERBOSE_MODE) {
-            log_trace("edit_batch printing batch mode correction chart.");
+            log_trace("(edit_batch) Printing batch mode correction chart.");
             shell_verbose_chart(lnum, line_copy, misspelled[i], suggestions);
         }
 
         j = 0;
+
         if (suggestions != NULL) {
             while (suggestions[j] != NULL) {
                 free(suggestions[j]);
+                
                 j++;
             }
+
             free(suggestions);
         }
 
@@ -93,15 +97,14 @@ char *edit_batch(char *line, dict_t *dict, int verbosity, int lnum) {
 
 /* See main_functions_batch.h */
 char **batch_mode(char *filename, dict_t *dict, bool *quit, int verbosity) {
-
     char **lines = parse_file(filename);
 
-    log_debug("batch_mode file parsed into a char array.");
+    log_debug("(batch_mode) File parsed into a char array.");
 
     // If lineparse_file returns NULL
     if (lines == NULL) {
         shell_error("Failed to parse file.", false);
-        log_error("batch_mode file parse failed.");
+        log_error("(batch_mode) file parse failed.");
         *quit = true;
 
         return NULL;
